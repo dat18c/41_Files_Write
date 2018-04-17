bb5d4b02a2cc346115584b60a1ea4294a53a61b7
JarlTuxen <jart@kea.dk>
2018-04-17 08:46:54 +0200

    file read exercise

---
+++ kode/FileReadWrite.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class FileReadWrite{
+
+   public static void main(String[] args) throws FileNotFoundException{
+   
+      // create File object
+      // create Scanner object
+      
+      // while loop så længe der er noget at læse - hint f.hasNext
+      // læs og skriv ud
+      
+   }
+
+}
