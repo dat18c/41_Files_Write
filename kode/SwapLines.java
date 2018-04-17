3263e5cc4bdd8c887d9aede2b467e014a2d7fae4
JarlTuxen <jart@kea.dk>
2018-04-17 11:18:30 +0200

    SwapLines exercise

---
+++ kode/SwapLines.java
@@ -0,0 +1,19 @@
+// lav javaprogram der tager fil og skriver med linierne linierne byttet om 2 og 2
+
+import java.io.*;
+import java.io.*;
+public class SwapLines{
+
+   public static void main(String[] args){
+   
+      //File
+      //Scanner - brug evt. inputScanner-metoden for at få input fra tastaturet
+      
+      //PrintStream
+      
+      //while flere linier
+      //   læs lines
+      //   skriv lines i omvendt rækkefølge
+   }
+
+}
