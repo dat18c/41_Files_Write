76dc113a70309f6426cd3c7d221a1db805874894
JarlTuxen <jart@kea.dk>
2018-04-17 09:52:16 +0200

    write to file

--- kode/FileReadWrite.java
+++ kode/FileReadWrite.java
@@ -6,10 +6,23 @@ public class FileReadWrite{
    public static void main(String[] args) throws FileNotFoundException{
    
       // create File object
+      File f = new File("data.txt");
       // create Scanner object
+      Scanner input = new Scanner(f);
       
-      // while loop så længe der er noget at læse - hint f.hasNext
-      // læs og skriv ud
+      // while loop så længe der er noget at læse - hint input.hasNext
+      while (input.hasNext()){
+         // læs og skriv ud
+         System.out.print(input.next());
+      }
+      
+      // create PrintStream
+      PrintStream output = new PrintStream("dataud.txt");
+      // skriv med println/print/printf...
+      output.println("Min første fil");
+      output.println("den næste linie!");
+      System.out.println("Min første fil er skrevet!");
+      
       
    }
 
