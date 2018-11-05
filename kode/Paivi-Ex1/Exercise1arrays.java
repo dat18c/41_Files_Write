0e6a41e81fad5739256f1b48193cf01da5b64faf
JarlTuxen <jart@kea.dk>
2018-11-05 10:49:28 +0100

    konstanter for dreng og pige

--- kode/Paivi-Ex1/Exercise1arrays.java
+++ kode/Paivi-Ex1/Exercise1arrays.java
@@ -5,8 +5,10 @@ public class Exercise1arrays
 {
 
    public static void main(String[] args)
-   throws FileNotFoundException
+      throws FileNotFoundException
    {
+      final int BOY = 0; //husk at drenge er index 0
+      final int GIRL = 1; //og pige er index 1
       
       //åbn fil og lav skanner 
       File f = new File("Exercise1");
@@ -29,15 +31,16 @@ public class Exercise1arrays
          //hvis modulus 0, dreng. Vi tæller antallet af piger og drenge. Vi regner summen     
          index = i % 2;
          taellerArray[index]++;
+         //læs næste int fra filen
          temp = input.nextInt();
          sumArray[index] += temp;
          i++ ;  
       }
     //vi printer summene og regne forskellen
     //print antal piger og drenge
-    System.out.println(taellerArray[0] + " drenge " + taellerArray[1] + " piger");
-    System.out.println("Drengesummen er: " + sumArray[0] + ". Pigesummen er: " + sumArray[1]);
-    forskel = Math.abs(sumArray[0]-sumArray[1]);
+    System.out.println(taellerArray[BOY] + " drenge " + taellerArray[GIRL] + " piger");
+    System.out.println("Drengesummen er: " + sumArray[BOY] + ". Pigesummen er: " + sumArray[GIRL]);
+    forskel = Math.abs(sumArray[BOY]-sumArray[GIRL]);
     System.out.println("Forskellen er: " + forskel);
    }
 }
