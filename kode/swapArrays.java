766efaed9133efa4c8514b3ed1b71d43761a803f
JarlTuxen <jart@kea.dk>
2018-11-09 09:49:40 +0100

    swapArrays

--- kode/swapArrays.java
+++ kode/swapArrays.java
@@ -8,14 +8,31 @@ public class swapArrays
       int[] tal1 = { 0, 2, 4, 6 };
       int[] tal2 = { 1, 3, 5, 7 };
       
-      //swap arrays - skriv metoden, der bytter om på elementerne i tal1 og tal2
-      //swapArrays(tal1, tal2);
       
       //udskriv arrays
       System.out.println("tal1: " + Arrays.toString(tal1));
       System.out.println("tal2: " + Arrays.toString(tal2));
+
+      //swap arrays - skriv metoden, der bytter om på elementerne i tal1 og tal2
+      swapArrays(tal1, tal2);
+
+      //udskriv arrays
+      System.out.println("tal1: " + Arrays.toString(tal1));
+      System.out.println("tal2: " + Arrays.toString(tal2));
+
    }
 
    //swapArrays - bytter elementer i arrayet
-
+   public static void swapArrays(int[] m, int[] n)
+   {
+      int temp = 0;
+      //loop igennem array og byt et element ad gangen
+      for (int i=0;i<m.length;i++)
+      {
+         temp = m[i];
+         m[i] = n[i];
+         n[i] = temp;
+      }
+   }
+   
 }
